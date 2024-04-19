e1e4a8491e8ed000af3d3119e2f41bbce1d2c7d5
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-19 11:12:47 -0300

    at

---
+++ at/atINW/src/componentes/navbar.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'; 
+
+function Navbar() {
+
+    return(
+        <div className='Navbar'>
+            <a><Link to='/'>Home</Link></a>
+            <a><Link to='/Sobre'>Sobre</Link></a>
+            <a><Link to='/Contato'>Contato</Link></a>
+        </div>
+    )
+}
+
+export default Navbar
