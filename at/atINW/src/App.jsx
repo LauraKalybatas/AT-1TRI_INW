e6366a35a665a5675ca3e7f67c4df7402029c781
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-22 10:13:26 -0300

    at

--- at/atINW/src/App.jsx
+++ at/atINW/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from './componentes/navbar.jsx'
+import MainContent from './componentes/MainContent'
 
 function App() {
   return (
     <>
       <Navbar/>
+      <MainContent/>
     </>
   )
 }
